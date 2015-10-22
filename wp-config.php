e0db42889c73b312cb3354c10fab864f66ca338f
gjohn78 <georgekjohn@att.net>
2015-10-22 03:24:52 -0500

    Updated connection info

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'worldwoAI3JNILdN');
+define('DB_NAME', 'worldwoAI3JNlLdN');
 
 /** MySQL database username */
 define('DB_USER', 'bf365ec07d409f');
